6585e4b28460be5f2dfa8870c4e8cd18a70e5818
Nihat Cakir <ckr.nihat@gmail.com>
2022-07-31 00:38:46 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import { AnimatePresence } from "framer-motion";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 
 import Fonts from "../styles/theming/fonts";
 import { theme } from "../styles/theming";
@@ -14,12 +14,18 @@ const client = new ApolloClient({
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <Fonts />
-        <AnimatePresence exitBeforeEnter>
-          <Component {...pageProps} />
-        </AnimatePresence>
-      </ChakraProvider>
+      <AnimatePresence exitBeforeEnter>
+        <ChakraProvider resetCSS theme={theme}>
+          <ColorModeProvider
+            options={{
+              useSystemColorMode: true,
+            }}
+          >
+            <Fonts />
+            <Component {...pageProps} />
+          </ColorModeProvider>
+        </ChakraProvider>
+      </AnimatePresence>
     </ApolloProvider>
   );
 }
